dda320de0c6a176339501b9e1aaedc71e9a265d9
Doungdara Keut <doungdara@yoolk.com>
2015-01-07 09:06:36 +0700

    update background

--- app/javascripts/initScrollTo.js
+++ app/javascripts/initScrollTo.js
@@ -16,8 +16,25 @@ scrollToResume = function(){
 
 // Function initializer
 initScrollTo = function() {
-  scrollToProfile();
-  scrollToResume();
+  // scrollToProfile();
+  // scrollToResume();
+  function onScroll(event){
+      var scrollPos = $(document).scrollTop();
+      $('.nav-content-list li a i ').each(function () {
+          var currLink = $(this);
+          console.log(currLink)
+          var refElement = $(currLink.attr("scroll"));
+          console.log(refElement)
+          if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+              $('.nav-content-list li a i').removeClass("active");
+              currLink.addClass("active");
+          }
+          else{
+              currLink.removeClass("active");
+          }
+      });
+  };
+  $(document).on("scroll", onScroll);
 };
 
 initScrollTo();
